c5989e8348e9be3849f6d023c7a7101dcee34b1c
Jared <realjaredeveritt@gmail.com>
2019-03-28 19:01:54 -0500

    Added Codes.txt which has a list of morse codes, and added basic functionality and file reading

--- src/mct/Main.java
+++ src/mct/Main.java
@@ -1,15 +1,18 @@
 package mct;
 
+import java.io.IOException;
+
 import javax.swing.JButton;
 import javax.swing.JTextField;
 
 public class Main {
 	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Window mainWindow = new Window();
 		mainWindow.setTitle("Morse Code Translator");
+		
 	}
 
 }
--- src/mct/Window.java
+++ src/mct/Window.java
@@ -3,6 +3,12 @@ package mct;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,6 +21,10 @@ public class Window extends JFrame {
 	public static JButton submit;
 	
 	public listener buttonListener;
+	
+	public int[] numArray;
+	
+	public File code;
 	
 	public Window() {
 		setSize(400, 400);
@@ -38,13 +48,37 @@ public class Window extends JFrame {
 		
 	}
 	
-	public class listener implements ActionListener {
+	public class listener implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
 			
 			if (e.getSource() == submit) {
-				morseText.setText("test");
-				System.out.println("test");
+				numArray = new int[80];
+				charToInt(englishText.getText());
+				morseText.setText("");
+				readMorseFile();
 			}
 		}
+	}
+	
+	public void charToInt(String s) {
+		char c;
+		for (short i = 0; i < s.length(); i++) {
+			c = s.charAt(i);
+			numArray[i] += ((int) c - 96);
+		}
+		
+		System.out.println(numArray[0]);
+	}
+	
+	public void readMorseFile() {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("Codes.txt"));
+			for (int j = 0; j < numArray[j]; j++) {
+				System.out.println(reader.readLine());
+			}
+		} catch(IOException e) {
+			System.out.println("Codes.txt file not found");
+		}
+		
 	}
 }
